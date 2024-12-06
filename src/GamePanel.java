e087db15faaa08de5314f73ed56485d304efb1f6
soniacodes <martsonia1@gmail.com>
2024-12-06 13:47:04 -0500

    Initalize snake game project, add grid rendering, snake movement, and apple generation, score keeping

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -71,7 +71,10 @@ public class GamePanel extends JPanel implements ActionListener {
                     g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                 }
             }
-
+            g.setColor(Color.red);
+            g.setFont( new Font("Ink Free",Font.BOLD, 40));
+            FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("Score: "+applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
         } else {
             gameOver(g);
         }
